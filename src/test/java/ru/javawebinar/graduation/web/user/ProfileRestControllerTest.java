3ef025f32a461b5d85436d0b745d20f6ebed6932
Skarakhod.OE <skorokhod.oe@nsd.ru>
2020-05-15 01:05:40 +0300

    update

--- src/test/java/ru/javawebinar/graduation/web/user/ProfileRestControllerTest.java
+++ src/test/java/ru/javawebinar/graduation/web/user/ProfileRestControllerTest.java
@@ -3,22 +3,14 @@ package ru.javawebinar.graduation.web.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import ru.javawebinar.graduation.model.User;
 import ru.javawebinar.graduation.service.UserService;
-import ru.javawebinar.graduation.to.UserTo;
-import ru.javawebinar.graduation.util.UserUtil;
 import ru.javawebinar.graduation.web.AbstractControllerTest;
-import ru.javawebinar.graduation.web.json.JsonUtil;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static ru.javawebinar.graduation.TestUtil.readFromJson;
 import static ru.javawebinar.graduation.TestUtil.userHttpBasic;
 import static ru.javawebinar.graduation.UserTestData.*;
-import static ru.javawebinar.graduation.util.exception.ErrorType.VALIDATION_ERROR;
 import static ru.javawebinar.graduation.web.user.ProfileRestController.REST_URL;
 
 
